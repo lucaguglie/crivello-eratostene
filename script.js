6a36c47aed55cc1a745419c6f9a8b0b4481329e4
xFabifont <fabifont.scuola@gmail.com>
2019-08-08 09:34:43 +0200

    Aggiunto confirm per le info e fixato un bug il quale non evidenziava l'ultimo primo

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let info =
+  'CRIVELLO DI ERATOSTENE\nInserire il numero massimo entro il quale cercare i numeri primi (da 2 al numero precedente a quello inserito) e scegliere una modalità di visualizzazione.\nNella modalità grafica si possono cambiare i colori premendo "Enter". Le celle bianche sono i numeri primi.\nLa modalità testuale riesce a calcolare oltre a un miliardo come cifra massima a differenza di quella grafica che dovendo renderizzare molte celle si ferma ad una cifra nettamente inferiore.\nSe preme "Ok" verrà impostato un cookie in modo tale che questo avviso non appaia più quando si refresha la pagina.'
 let modeChanged = false
 let oldMax = 0
 let primi = []
+
+InitPopup()
 
 function ChangeMode() {
   modeChanged = true
@@ -65,14 +69,13 @@ function Pulisci() {
 }
 
 function Calcola() {
-
-  let max = document.getElementById('max').value 
+  let max = document.getElementById('max').value
+  
   if (max == '') {
     oldMax = 0
     Pulisci()
-    return false 
-  }
-  else if (isNaN(max) || (!isNaN(max) && parseInt(max) < 1)) {
+    return false
+  } else if (isNaN(max) || (!isNaN(max) && parseInt(max) < 1)) {
     oldMax = 0
     alert('I caratteri e i numeri minori di 1 non sono ammessi')
     return false
@@ -80,10 +83,11 @@ function Calcola() {
 
   max = parseInt(max)
 
-  if(oldMax == max && modeChanged == false && event.key == 'Enter') {
+  if (oldMax == max && modeChanged == false && event.key == 'Enter') {
     Colors(primi)
     return false
-  } else if(oldMax == max && modeChanged == false && event.key != 'Enter') return false
+  } else if (oldMax == max && modeChanged == false && event.key != 'Enter')
+    return false
 
   Pulisci()
 
@@ -102,8 +106,9 @@ function Calcola() {
 
   // Trovo i numeri primi partendo da 2, 3, 5...
   for (let i = 2; i <= upperLimit; i++) {
-    if (array[i]) { // È un numero primo
-      for (let j = i * i; j < max; j += i) {
+    if (array[i]) {
+      // È un numero primo
+      for (let j = i * i; j <= max; j += i) {
         if (mode == 'true')
           // Assegno la classe con il numero primo per colorare la cella in seguito
           tds[j - 1].setAttribute('class', i)
@@ -115,10 +120,8 @@ function Calcola() {
 
   // Modalità testuale
   if (mode == 'false')
-    document.getElementById('primi').innerHTML = Primi(array, max).join(
-      ', '
-    )
-  else Colors(primi = Primi(array, max))
+    document.getElementById('primi').innerHTML = Primi(array, max).join(', ')
+  else Colors((primi = Primi(array, max)))
 
   oldMax = max
   modeChanged = false
@@ -146,5 +149,41 @@ function Colors(primi) {
     for (let i = 0; i < tds.length; i++) {
       tds[i].setAttribute('bgColor', color)
     }
-  });
+  })
+}
+
+function SetCookie(name, value) {
+  let expiration = new Date()
+  let now = new Date()
+  expiration.setTime(now.getTime() + 365 * 24 * 60 * 60 * 1000) // un anno
+  document.cookie =
+    name +
+    '=' +
+    escape(value) +
+    '; expires=' +
+    expiration.toGMTString() +
+    '; path=/'
+}
+
+function CheckCookie(name) {
+  if (document.cookie.length > 0) {
+    let start = document.cookie.indexOf(name + '=')
+    if (start != -1) {
+      start = start + name.length + 1
+      let end = document.cookie.indexOf(';', start)
+      if (end == -1) end = document.cookie.length
+      return true
+    } else {
+      return false
+    }
+  }
+  return false
+}
+
+function DeleteCookie(name) {
+  document.cookie = name + '=; Max-Age=-99999999;'
+}
+
+function InitPopup() {
+  if (!CheckCookie('pop')) if (confirm(info)) SetCookie('pop', 0)
 }
